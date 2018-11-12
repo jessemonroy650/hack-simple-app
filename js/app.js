0867ad6e0ea9c970397c4930298f67a79fec8557
jessemonroy650 <jessemonroy650@yahoo.com>
2018-11-11 19:09:56 -0700

    try more

--- js/app.js
+++ js/app.js
@@ -6,9 +6,10 @@ var app = {
 
     //
     onDOMContentLoaded : function () {
+        alert("onDOMContentLoaded");
         document.getElementById('originalImage').src = 'img/dad+benny.jpg';
         document.getElementById('test').innerHTML    = 'app.onDOMContentLoaded';
 
-        alert("onDOMContentLoaded");
+        alert("just after last statement in `onDOMContentLoaded`");
     }
 };
