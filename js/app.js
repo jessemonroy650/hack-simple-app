d41997af8cd96ca2b0a8172f6ae329b61920895d
jessemonroy650 <jessemonroy650@yahoo.com>
2018-11-11 20:41:09 -0700

    try `cli-7.0.1`

--- js/app.js
+++ js/app.js
@@ -1,15 +1,8 @@
 //
 //
 //
-var app = {
-    version : '1.0.0',
-
-    //
-    onDOMContentLoaded : function () {
+function onDOMContentLoaded() {
         alert("onDOMContentLoaded");
         document.getElementById('originalImage').src = 'img/dad+benny.jpg';
         document.getElementById('test').innerHTML    = 'app.onDOMContentLoaded';
-
-        alert("just after last statement in `onDOMContentLoaded`");
-    }
 };
